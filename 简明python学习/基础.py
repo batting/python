eba848292bac5f32ec9722e7e6cc9cf5bc40ab92
batting <674201281@qq.com>
2019-01-08 16:16:07 +0800

    更新了fomat用法

--- 简明python学习/基础.py
+++ 简明python学习/基础.py
@@ -9,3 +9,9 @@ adasd
 asdad
 asdasd
 ''')#三引号用法
+
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
